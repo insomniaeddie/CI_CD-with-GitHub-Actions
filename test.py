8063e8444b40fa00ef1ae09182b07faa132bdc9a
Eddie C <insomina137@gmail.com>
2023-03-12 17:48:02 -0400

    Updated file

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from calculator import add, subtract, multiply, divide  # noqa
 # define a test function to test the add function
 
 
-def test_add():
+def Test_add():
     # use subprocess to run the calculator app with arguments for adding 2 and 3
     result = subprocess.check_output(
         ['python', 'calculator.py', 'add', '2', '3'])
@@ -17,21 +17,21 @@ def test_add():
 # define a test function to test the subtract function
 
 
-def test_subtract():
+def Test_subtract():
     result = subprocess.check_output(
         ['python', 'calculator.py', 'subtract', '10', '5'])
     assert int(result) == 5
 
 
 # define a test function to test the multiply function
-def test_multiply():
+def Test_multiply():
     result = subprocess.check_output(
         ['python', 'calculator.py', 'multiply', '2', '3'])
     assert int(result) == 6
 
 
 # define a test function to test the divide function
-def test_divide():
+def Test_divide():
     result = subprocess.check_output(
         ['python', 'calculator.py', 'divide', '10', '2'])
     assert int(result) == 5
